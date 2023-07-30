454f63f40985a29a0ad63bbf4e9aa2fc18282cb7
Rakib Firoz <rakib.work96@gmail.com>
2023-07-30 08:49:56 +0000

    Copilot second commit

---
+++ number.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsmemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: 'm'
+        }
+        };
+    }
+}
